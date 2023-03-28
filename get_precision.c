5a475b497c601ef7df1c4cf9e280917e4e6f2e28
Quedjo <85486255+Quedjo@users.noreply.github.com>
2023-03-28 18:08:45 +0000

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 
@@ -71,64 +72,5 @@ int get_precision(const char *format, int *i, va_list list)
 
 
         return (precision);
-
-}
-
-
-
-========================================
-
-
-
-get_size.c CODE
-
-
-
-
-#include "main.h"
-
-
-/**
-
- * get_size - Calculates the size to cast the argument
-
- * @format: Formatted string in which to print the arguments
-
- * @i: List of arguments to be printed.
-
- *
-
- * Return: Precision.
-
- */
-
-int get_size(const char *format, int *i)
-
-{
-
-        int curr_i = *i + 1;
-
-        int size = 0;
-
-
-        if (format[curr_i] == 'l')
-
-                size = S_LONG;
-
-        else if (format[curr_i] == 'h')
-
-                size = S_SHORT;
-
-
-        if (size == 0)
-
-                *i = curr_i - 1;
-
-        else
-
-                *i = curr_i;
-
-
-        return (size);
 
 }
